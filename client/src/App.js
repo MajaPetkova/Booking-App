72331099d20d8e5748d1886b19a30a770410c8b3
Maja Petkova <majapetkova1@yahoo.com>
2023-07-21 11:50:33 +0200

    Added components for home, listing and single hotel

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
-
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { Home } from "./components/home/Home";
+import { List } from "./components/list/List";
+import { Hotel } from "./components/hotel/Hotel";
 
 function App() {
   return (
-    <div className="App">
-    Hello World
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} ></Route>
+      <Route path="/hotels" element={<List/>}></Route>
+      <Route path="/hotels/:id" element={<Hotel/>}></Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
