2171235bbda10ae93d0592e1a0ddfd638cc898ac
Maja Petkova <majapetkova1@yahoo.com>
2023-07-23 20:45:29 +0200

    Update home jsx and css, add feature jsx and css

---
+++ client/src/components/featured/Featured.jsx
@@ -0,0 +1,27 @@
+import "./featured.css";
+
+export const Featured = () => {
+  return <div className="featured">
+    <div className="featuredItem">
+        <img src="https://images.unsplash.com/photo-1512470876302-972faa2aa9a4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8YW1zdGVyZGFtfGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60" alt="" className="featuredImg"/>
+        <div className="featuredTitles">
+          <h1>Amsterdam</h1>  
+          <h2>123 properties</h2>
+        </div>
+    </div>
+    <div className="featuredItem">
+        <img src="https://images.unsplash.com/photo-1431274172761-fca41d930114?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cGFyaXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60" alt="" className="featuredImg"/>
+        <div className="featuredTitles">
+          <h1>Paris</h1>  
+          <h2>123 properties</h2>
+        </div>
+    </div>
+    <div className="featuredItem">
+        <img src="https://images.unsplash.com/photo-1572202378710-71d8b1cce96c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cm90dGVyZGFtfGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60" alt="" className="featuredImg"/>
+        <div className="featuredTitles">
+          <h1>Rotterdam</h1>  
+          <h2>123 properties</h2>
+        </div>
+    </div>
+     </div>;
+};
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,12 +1,18 @@
-import { Header } from "../../components/header/Header"
-import { Navbar } from "../../components/navbar/Navbar"
-import "./home.css"
+import { Featured } from "../../components/featured/Featured";
+import { Header } from "../../components/header/Header";
+import { Navbar } from "../../components/navbar/Navbar";
 
-export const Home=()=>{
-    return (
-        <div><Navbar/>
-        <Header/>
-        </div>
-    
-    )
-}
+import "./home.css";
+
+export const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <div className="homeContainer">
+        <Featured/>
+        <h1 className="homeTitle">Browse by property type</h1>
+      </div>
+    </div>
+  );
+};
