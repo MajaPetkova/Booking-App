46c712927f8cd6a6ddc4db112af4d630d083fd21
Maja Petkova <majapetkova1@yahoo.com>
2023-07-23 21:03:32 +0200

    Add property list jsx and css

---
+++ client/src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,34 @@
+import "./propertyList.css";
+
+export const Propertylist = () => {
+  return <div className="propertyList">
+    <div className="pItem">
+        <img src="https://images.unsplash.com/photo-1444201983204-c43cbd584d93?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjF8fGhvdGVsfGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60" alt="" className="pListImg" />
+       <div className="pListTitles">
+         <h1>Hotels</h1>
+         <h2>233 Hotels</h2>
+       </div>
+    </div>
+    <div className="pItem">
+        <img src="https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60" alt="" className="pListImg" />
+       <div className="pListTitles">
+         <h1>Apartments</h1>
+         <h2>263 Hotels</h2>
+       </div>
+    </div>
+    <div className="pItem">
+        <img src="https://images.unsplash.com/photo-1584132967334-10e028bd69f7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cmVzb3J0c3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60" alt="" className="pListImg" />
+       <div className="pListTitles">
+         <h1>Resorts</h1>
+         <h2>548 Hotels</h2>
+       </div>
+    </div>
+    <div className="pItem">
+        <img src="https://images.unsplash.com/photo-1564013799919-ab600027ffc6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8dmlsbGFzfGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60" alt="" className="pListImg" />
+       <div className="pListTitles">
+         <h1>Villas</h1>
+         <h2>703 Hotels</h2>
+       </div>
+    </div>
+  </div>;
+};
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import { Featured } from "../../components/featured/Featured";
 import { Header } from "../../components/header/Header";
 import { Navbar } from "../../components/navbar/Navbar";
+import { Propertylist } from "../../components/propertyList/PropertyList";
 
 import "./home.css";
 
@@ -12,6 +13,7 @@ export const Home = () => {
       <div className="homeContainer">
         <Featured/>
         <h1 className="homeTitle">Browse by property type</h1>
+        <Propertylist/>
       </div>
     </div>
   );
