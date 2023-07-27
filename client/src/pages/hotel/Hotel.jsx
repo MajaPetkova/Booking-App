86c5df3ca6958bc22d54ddd0f3ecab66bcb47294
Maja Petkova <majapetkova1@yahoo.com>
2023-07-27 22:31:02 +0200

    Added jsx for single hotel

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -1,6 +1,52 @@
+import { Header } from "../../components/header/Header";
+import { Navbar } from "../../components/navbar/Navbar";
+import { GoLocation } from "react-icons/go";
+import "./hotel.css";
 
-export const Hotel=()=>{
-    return (
-        <div>Hotel</div>
-    )
-}
+export const Hotel = () => {
+  const photos = [
+    {
+      src: "https://images.unsplash.com/photo-1679678691263-cc7f30ce02f8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxzZWFyY2h8MXx8aG90ZWx8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1496417263034-38ec4f0b665a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aG90ZWx8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8aG90ZWx8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1596701062351-8c2c14d1fdd0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fGhvdGVsfGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60",
+    },
+    {
+      src: "https://plus.unsplash.com/premium_photo-1678297270891-fda2e16796ea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG90ZWwlMjByb29tfGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60",
+    },
+  ];
+  return (
+    <div>
+      <Navbar />
+      <Header type="list" />
+      <div className="hotelContainer">
+        <div className="hotelWrapper">
+          <h1 className="hotelTitle">Grand Hotel</h1>
+          <div className="hotelAddress">
+            <GoLocation />
+            <span>Elton Str 124, New York </span>
+          </div>
+          <span className="hotelDistance">
+            Excellent Location - 500m from center
+          </span>
+          <span className="hotelPriceHighlight">
+            Book a stay over $114 at this property and get a free airport taxi
+          </span>
+          <div className="hotelImages">
+            {photos.map(x=>{
+                return(               <div className="hotelImgWrapper">
+                <img src={x.src} alt=""  className="hotelImg"/>
+               </div>)
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
