b91ddb775eb004ca3f10b02982e0dedc7827f626
Maja Petkova <majapetkova1@yahoo.com>
2023-08-08 15:41:59 +0200

    remove env, add verifying Token

--- api/controllers/authentication.js
+++ api/controllers/authentication.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+const createError = require("../utils.js/error");
 
 const register = async (req, res, next) => {
   try {
@@ -23,16 +24,16 @@ const login = async (req, res, next) => {
   try {
     const user = await User.findOne({ username: req.body.username });
     if (!user) {
-      return next(new Error("User not found"));
+      return next(createError(404,"User not found"));
     }
     const isPasswordCorrect = await bcrypt.compare(
       req.body.password,
       user.password
     );
     if (!isPasswordCorrect) {
-      return next(new Error( "Username or Password are not correct"));
+      return next(createError( "Username or Password are not correct"));
     }
-    const token= jwt.sign({id:user._id, isAdmin:user.isAdmin}, process.env.JWT );
+    const token= jwt.sign({id:user._id, isAdmin:user.isAdmin}, "fgcjkjl;'kkvghhjgk" );
 
     const{password, hashedPassword, ...otherDetails}= user._doc;
     res.cookie("access_token", token, {httpOnly: true}).status(200).json({...otherDetails});
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const { getAllUsers, getUser, updateUser, deleteUser } = require("../controllers/userController");
+const verifyToken = require("../utils.js/verifyToken");
 const router = express.Router();
 
+router.get("/checkAuthentication", verifyToken, (req, res, next)=>{
+    res.send("Hello User, you are authenticated")
+})
 router.get("/", getAllUsers);
 router.get("/:id", getUser);
 router.put("/:id", updateUser);
