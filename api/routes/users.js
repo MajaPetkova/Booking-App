8e80cc55484780907f08e96e55a3184ca732da6c
Maja Petkova <majapetkova1@yahoo.com>
2023-08-27 14:50:19 +0200

    Add verify User with token

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,16 +1,23 @@
 const express = require("express");
-const { getAllUsers, getUser, updateUser, deleteUser } = require("../controllers/userController");
-const verifyToken = require("../utils.js/verifyToken");
+const {
+  getAllUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/userController");
+const { verifyToken, verifyUser } = require("../utils.js/verifyToken");
+
 const router = express.Router();
 
-router.get("/checkAuthentication", verifyToken, (req, res, next)=>{
-    res.send("Hello User, you are authenticated")
-})
+router.get("/checkAuthentication", verifyToken, (req, res, next) => {
+  res.send("Hello User, you are authenticated");
+});
+router.get("/checkUser/:id", verifyUser, (req, res, next) => {
+  res.send("Hello User, you are authenticated and you can delete your account");
+});
 router.get("/", getAllUsers);
 router.get("/:id", getUser);
 router.put("/:id", updateUser);
-router.delete("/:id", deleteUser)
-
-
+router.delete("/:id", deleteUser);
 
 module.exports = router;
