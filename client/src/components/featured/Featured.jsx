a3b277eb1a3241f942b2eed4c46558e61ffd0a86
Maja Petkova <majapetkova1@yahoo.com>
2023-08-28 11:26:11 +0200

    Fetching data from url

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,6 +1,11 @@
+import { useFetch } from "../../hooks/useFetch";
 import "./featured.css";
 
 export const Featured = () => {
+
+  const {data, loading, error} = useFetch("http://localhost:5000/api/hotels/countByCity?cities=berlin,Barcelona,Bitola")
+  console.log(data)
+
   return <div className="featured">
     <div className="featuredItem">
         <img src="https://images.unsplash.com/photo-1512470876302-972faa2aa9a4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8YW1zdGVyZGFtfGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60" alt="" className="featuredImg"/>
---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get("http://localhost:5000/api/hotels/countByCity?cities=berlin,Barcelona,Bitola");
+        setData(res.data);
+        // console.log(res.data);
+      } catch (err) {
+        setError(err);
+      }
+      setLoading(false)
+    };
+    fetchData();
+  }, [url]);
+  const reFetch = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(url);
+        setData(res.data);
+        // console.log(res.data);
+        } catch (err) {
+            setError(err);
+        }
+        setLoading(false)
+    };
+    // reFetch();
+    
+return {data, loading, error, reFetch}
+};
