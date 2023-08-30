4f554deb12a02e8863ff2618a35f502fbeeeb7d6
Maja Petkova <majapetkova1@yahoo.com>
2023-08-30 23:13:28 +0200

    Add link to navbar, css and update propertyList.jsx

--- client/src/components/propertyList/PropertyList.jsx
+++ client/src/components/propertyList/PropertyList.jsx
@@ -3,7 +3,7 @@ import "./propertyList.css";
 
 export const Propertylist = () => {
   const { data, loading, error } = useFetch("http://localhost:5000/api/hotels/countByType");
- console.log(data[0].count)
+  
   const images = [
     "https://images.unsplash.com/photo-1444201983204-c43cbd584d93?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjF8fGhvdGVsfGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60",
     "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60",
@@ -18,11 +18,11 @@ export const Propertylist = () => {
         <>
           {data &&
             images.map((x, i) => (
-              <div className="pItem">
+              <div className="pItem" key={i}>
                 <img src={x} alt="" className="pListImg" />
                 <div className="pListTitles">
-                   <h1>{data[i].type}</h1>
-               <h2> {data[i].count} {data[i].type}</h2> 
+                   <h1>{data[i]?.type}</h1>
+               <h2> {data[i]?.count} {data[i]?.type}</h2> 
                 </div>
               </div>) )}
         </>
