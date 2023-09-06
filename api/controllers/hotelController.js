fa0727d500e45e7cb094475257b550493a434363
Maja Petkova <majapetkova1@yahoo.com>
2023-09-06 11:16:36 +0200

    Fetching data using URL location

--- api/controllers/hotelController.js
+++ api/controllers/hotelController.js
@@ -44,9 +44,12 @@ const getHotel = async (req, res, next) => {
   }
 };
 const getAllHotels = async (req, res, next) => {
-  const {min,max, ...others} = req.query;
+  const { min, max, ...others } = req.query;
   try {
-    const hotels = await Hotel.find({...others, cheapestPrice: {$gt:min | 1 , $lt: max || 999} });
+    const hotels = await Hotel.find({
+      ...others,
+      cheapestPrice: { $gt: min | 1, $lt: max || 999 },
+    });
     res.status(200).json(hotels);
   } catch (err) {
     // res.status(500).json(err);
@@ -56,9 +59,11 @@ const getAllHotels = async (req, res, next) => {
 const countByCity = async (req, res, next) => {
   const cities = req.query.cities.split(",");
   try {
-    const list = await Promise.all(cities.map(city=>{
-      return Hotel.countDocuments({city:city})
-    }));
+    const list = await Promise.all(
+      cities.map((city) => {
+        return Hotel.countDocuments({ city: city });
+      })
+    );
     res.status(200).json(list);
   } catch (err) {
     // res.status(500).json(err);
@@ -67,18 +72,18 @@ const countByCity = async (req, res, next) => {
 };
 const countByType = async (req, res, next) => {
   try {
-  const hotelCount =await Hotel.countDocuments({type:"hotel"});
-  const apartmentCount =await Hotel.countDocuments({type:"apartment"});
-  const resortCount =await Hotel.countDocuments({type:"resort"});
-  const villaCount =await Hotel.countDocuments({type:"villa"});
-  const cabinCount =await Hotel.countDocuments({type:"cabin"});
+    const hotelCount = await Hotel.countDocuments({ type: "hotel" });
+    const apartmentCount = await Hotel.countDocuments({ type: "apartment" });
+    const resortCount = await Hotel.countDocuments({ type: "resort" });
+    const villaCount = await Hotel.countDocuments({ type: "villa" });
+    const cabinCount = await Hotel.countDocuments({ type: "cabin" });
 
     res.status(200).json([
-      {type:"hotels", count:hotelCount},
-      {type:"apartments", count:apartmentCount},
-      {type:"resorts", count:resortCount},
-      {type:"villas", count:villaCount},
-      {type:"cabins", count:cabinCount},
+      { type: "hotels", count: hotelCount },
+      { type: "apartments", count: apartmentCount },
+      { type: "resorts", count: resortCount },
+      { type: "villas", count: villaCount },
+      { type: "cabins", count: cabinCount },
     ]);
   } catch (err) {
     // res.status(500).json(err);
@@ -92,5 +97,6 @@ module.exports = {
   deleteHotel,
   getHotel,
   getAllHotels,
-  countByCity, countByType
+  countByCity,
+  countByType,
 };
--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -6,30 +6,36 @@ import { MailList } from "../../components/mail-list/MailList";
 import { Footer } from "../../components/footer/Footer";
 import { useState } from "react";
 import { FaArrowLeft, FaArrowRight, FaRegWindowClose } from "react-icons/fa";
+import { useLocation } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
 
 export const Hotel = () => {
+  const location = useLocation()
+  const id= location.pathname.split("/")[2];
   const [slideNumber, setSlideNumber] = useState(0);
   const [openImg, setOpenImg] = useState(false);
-  const photos = [
-    {
-      src: "https://images.unsplash.com/photo-1679678691263-cc7f30ce02f8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxzZWFyY2h8MXx8aG90ZWx8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
-    },
-    {
-      src: "https://images.unsplash.com/photo-1496417263034-38ec4f0b665a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aG90ZWx8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
-    },
-    {
-      src: "https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8aG90ZWx8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
-    },
-    {
-      src: "https://images.unsplash.com/photo-1596701062351-8c2c14d1fdd0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fGhvdGVsfGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60",
-    },
-    {
-      src: "https://plus.unsplash.com/premium_photo-1678297270891-fda2e16796ea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG90ZWwlMjByb29tfGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60",
-    },
-    {
-      src: "https://images.unsplash.com/photo-1587527901949-ab0341697c1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGhvdGVsJTIwYmF0aHJvb218ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
-    },
-  ];
+
+  const {data, loading, error} = useFetch(`http://localhost:5000/api/hotels/find/${id}`)
+  // const photos = [
+  //   {
+  //     src: "https://images.unsplash.com/photo-1679678691263-cc7f30ce02f8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxzZWFyY2h8MXx8aG90ZWx8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
+  //   },
+  //   {
+  //     src: "https://images.unsplash.com/photo-1496417263034-38ec4f0b665a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aG90ZWx8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
+  //   },
+  //   {
+  //     src: "https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8aG90ZWx8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
+  //   },
+  //   {
+  //     src: "https://images.unsplash.com/photo-1596701062351-8c2c14d1fdd0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fGhvdGVsfGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60",
+  //   },
+  //   {
+  //     src: "https://plus.unsplash.com/premium_photo-1678297270891-fda2e16796ea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG90ZWwlMjByb29tfGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60",
+  //   },
+  //   {
+  //     src: "https://images.unsplash.com/photo-1587527901949-ab0341697c1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGhvdGVsJTIwYmF0aHJvb218ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
+  //   },
+  // ];
   const handleOpen = (i) => {
     setSlideNumber(i);
     setOpenImg(true);
@@ -50,13 +56,15 @@ export const Hotel = () => {
     <div>
       <Navbar />
       <Header type="list" />
-      <div className="hotelContainer">
+      {loading ? "Loading..." : (<div className="hotelContainer">
         {openImg && (
           <div className="slider">
             <FaRegWindowClose className="closeBtn" onClick={closeSlider} />
             <FaArrowLeft className="arrowBtn" onClick={() => handleMove("l")} />
             <div className="sliderWrapper">
-              <img src={photos[slideNumber].src} alt="hotelImg" />
+              {/* <img src={photos[slideNumber].src} alt="hotelImg" /> */}
+              <img src={data.photos[slideNumber]} alt="hotelImg" />
+
             </div>
             <FaArrowRight
               className="arrowBtn"
@@ -66,23 +74,23 @@ export const Hotel = () => {
         )}
         <div className="hotelWrapper">
           <button className="book">Reserve or Book Now!</button>
-          <h1 className="hotelTitle">Grand Hotel</h1>
+          <h1 className="hotelTitle">{data.name}</h1>
           <div className="hotelAddress">
             <GoLocation />
-            <span>Elton Str 124, New York </span>
+            <span>{data.address} </span>
           </div>
           <span className="hotelDistance">
-            Excellent Location - 500m from center
+            Excellent Location - ${data.distance} from center
           </span>
           <span className="hotelPriceHighlight">
-            Book a stay over $114 at this property and get a free airport taxi
+            Book a stay over ${data.cheapestPrice} at this property and get a free airport taxi
           </span>
           <div className="hotelImages">
-            {photos.map((x, i) => {
+            {data.photos?.map((x, i) => {
               return (
-                <div className="hotelImgWrapper">
+                <div className="hotelImgWrapper" key={i}>
                   <img
-                    src={x.src}
+                    src={x}
                     alt=""
                     className="hotelImg"
                     onClick={() => handleOpen(i)}
@@ -93,17 +101,9 @@ export const Hotel = () => {
           </div>
           <div className="hotelDetails">
             <div className="hotelDetailsText">
-              <h2 className="hotelTitle">Stay in the heart of New York</h2>
+              <h2 className="hotelTitle">{data.title}</h2>
               <p className="hotelDescription">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Dolorum quis nihil mollitia autem ullam, delectus obcaecati
-                assumenda quas iure expedita libero amet veritatis voluptatem
-                suscipit fugiat officiis saepe esse rem? Accusantium corrupti
-                facilis earum quasi obcaecati id maiores sequi quia molestiae
-                quas expedita eligendi cupiditate dolor quis impedit suscipit
-                laboriosam tempore animi rerum, quaerat sapiente possimus ad.
-                Praesentium ea distinctio quas tempora ipsa mollitia dolorum
-                explicabo, veniam fugit porro rerum!
+            {data.desc}
               </p>
             </div>
             <div className="hotelDetailsPrice">
@@ -119,7 +119,7 @@ export const Hotel = () => {
             </div>
           </div>
         </div>
-      </div>
+      </div>)}
       <MailList />
       <Footer />
     </div>
