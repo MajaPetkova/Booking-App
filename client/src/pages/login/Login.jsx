c28469b09975411742522db48feedc91cd1374ed
Maja Petkova <majapetkova1@yahoo.com>
2023-09-07 11:38:33 +0200

    Added Context to Navbar and display buttons if it's not user, add useNavigate hook

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -2,14 +2,15 @@ import axios from "axios";
 import { AuthContext } from "../../context/AuthContext";
 import "./login.css";
 import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
   const [credentials, setCredentials] = useState({
     username: undefined,
     password: undefined,
   });
-  const { user, loading, error, dispatch } = useContext(AuthContext);
-
+  const { loading, error, dispatch } = useContext(AuthContext);
+  const navigate= useNavigate()
 
   const handleChange = (e)=>{
      setCredentials(prev => ({
@@ -23,18 +24,19 @@ export const Login = () => {
      try{
      const res = await axios.post("http://localhost:5000/api/auth/login", credentials);
      dispatch({type: "LOGIN_SUCCESS", payload: res.data})
-
+     navigate("/")
      }catch(err){
        dispatch({type:"LOGIN_FAILURE", payload: err.response.data}) 
      }
   }
-     console.log(user)
+   
   return (
     <div className="login">
+        <h2>Login</h2>
       <div className="loginContainer">
         <input type="text" className="loginInput" placeholder="username" id="username" onChange={handleChange}/>
         <input type="password" className="loginInput" placeholder="password" id="password" onChange={handleChange}/>
-        <button className="loginBtn" onClick={handleClick}>Login</button>
+        <button disabled={loading} className="loginBtn" onClick={handleClick}>Login</button>
         {error && <span>{error.message}</span>}
       </div>
     </div>
